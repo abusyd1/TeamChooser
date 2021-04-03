18e5b5c46288050f5500983d80a73894aa36d26d
Sayeed <sayeed@sayeed.com>
2021-04-03 15:32:38 +0000

    dfj

--- db/database.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS `Players` (
-	`name` VARCHAR(50) NOT NULL,
-	`position` VARCHAR(50) NOT NULL,
-	`attack_rating` INT NOT NULL,
-	`defense_rating` INT, NOT NULL
-	`runner` BOOLEAN DEFAULT 'False'
-);
