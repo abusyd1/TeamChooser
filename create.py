538ef0b195ab32e81eec70e92e453c42f65b3ee7
Sayeed <sayeed@sayeed.com>
2021-04-03 13:39:55 +0000

    Inital setup working

--- create.py
+++ create.py
@@ -6,8 +6,8 @@ from application.models import Players
 db.drop_all()
 db.create_all()
 db.session.add_all([
-    Players(name = "Hussain Ahmed", attack_rating = 90), defense_rating = 60, runner = False)
-    Players(name = "Abul Shaher", attack_rating = 75), defense_rating = 85, runner = True)
-    Players(name = "Jamiul Hoque", attack_rating = 85), defense_rating = 70, runner = False)
-    Players(name = "Abu Shuu", attack_rating = 70), defense_rating = 80, runner = False)
+    Players(name = "Hussain Ahmed", position = "Forward", attack_rating = 90, defense_rating = 60, runner = False),
+    Players(name = "Abul Shaher", position = "Defence", attack_rating = 75, defense_rating = 85, runner = True),
+    Players(name = "Jamiul Hoque", position = "Forward", attack_rating = 85, defense_rating = 70, runner = False),
+    Players(name = "Abu Shuu", position = "Defence", attack_rating = 70, defense_rating = 80, runner = False)])
 db.session.commit()
