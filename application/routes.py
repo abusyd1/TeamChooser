6c7ac7bca6f0687bc73d11aebb0d8e4853ff4976
Sayeed <sayeed@sayeed.com>
2021-04-03 18:58:43 +0000

    other method

--- application/routes.py
+++ application/routes.py
@@ -2,7 +2,7 @@
 
 from application import app, db
 from application.models import Players
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
@@ -13,7 +13,15 @@ def players():
     if request.method =="GET":
        return render_template("players.html", query=Players.query.all())
 
-@app.route('/team1', methods=['GET', 'POST'])
+@app.route('/team2', methods=['GET', 'POST'])
 def team1():
+    query = Players.query.all()
+    team1 = []
+    team2 = []
+    for player in query:
+        if player.attack_rating > 85:
+            team1.append(player.name)
+        else:
+            team2.append(player.name)
     if request.method =="GET":
-       return render_template("team1.html", query=Players.query.all())
+       return render_template("team2.html", team1=team1, team2=team2)
